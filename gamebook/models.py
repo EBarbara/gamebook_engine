6be404745a4fd38b6e5ad49943bf00b29ebb03bb
Estevan Barbará <estevan.barbara@terrena.tech>
2025-06-21 09:03:23 -0300

    Base models

--- gamebook/models.py
+++ gamebook/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Gamebook (models.Model):
+    code = models.SlugField(unique=True)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Paragraph (models.Model):
+    gamebook = models.ForeignKey(Gamebook, on_delete=models.CASCADE, related_name='paragraphs')
+    number = models.IntegerField()
+    text = models.TextField()
+    combat_enemy = models.CharField(max_length=100, null=True, blank=True)
+    combat_skill = models.IntegerField(null=True, blank=True)
+    combat_stamina = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('gamebook', 'number')
+
+class Choice (models.Model):
+    paragraph = models.ForeignKey(Paragraph, on_delete=models.CASCADE, related_name='choices')
+    text = models.TextField(max_length=300)
+    target = models.IntegerField()
