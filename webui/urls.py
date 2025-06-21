3c0373cea2cc3a1847fe2a472792ca85e3d2956b
Estevan Barbará <estevan.barbara@terrena.tech>
2025-06-21 18:33:29 -0300

    Exibição básica de parágrafos

--- webui/urls.py
+++ webui/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import gamebook_list
+from .views import gamebook_list, read_paragraph
 
 urlpatterns = [
     path('gamebooks/', gamebook_list, name='gamebook-list'),
+    path('gamebooks/<str:code>/read/<int:number>', read_paragraph, name='read-paragraph'),
 ]
--- webui/views.py
+++ webui/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from gamebooks.models import Gamebook
+from gamebooks.models import Gamebook, Paragraph
 
 
 def gamebook_list(request):
     books = Gamebook.objects.all()
     return render(request, 'webui/gamebook_list.html', {'books': books})
+
+def read_paragraph(request, code, number):
+    book = get_object_or_404(Gamebook, code=code)
+    paragraph = get_object_or_404(Paragraph, gamebook=book, number=number)
+    return render(request, 'webui/read_paragraph.html', {'book': book, 'paragraph': paragraph})
